f31df3b47ec37b736468cd231245ab26460dd1b5
Jyotsna03 <jyotsnabh03@gmail.com>
2024-09-18 23:49:34 +0530

    Added Firebase for  Login

--- cleverwealth/src/App.js
+++ cleverwealth/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes, useLocation } from 'react-router-dom';
 import CompanyPage from './components/CompanyPage';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
@@ -10,10 +10,14 @@ import UseCasesPage from './components/UseCasesPage';
 import SignUpForm from './components/SignUpForm';
 
 function App() {
-  return (
+  const location = useLocation(); // Get the current route
 
-    <Router>
-      <Navbar />
+  // Only show Navbar if the current path is NOT "/signup"
+  const shouldShowNavbar = location.pathname !== '/signup';
+
+  return (
+    <>
+      {shouldShowNavbar && <Navbar />}  {/* Conditionally render Navbar */}
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -23,11 +27,15 @@ function App() {
         <Route path="/usecases" element={<UseCasesPage />} />
         <Route path="/pricing" element={<PricingPage />} />
         <Route path="/company" element={<CompanyPage />} />
-
       </Routes>
-    </Router>
-
+    </>
   );
 }
 
-export default App;
+export default function AppWrapper() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
--- cleverwealth/src/components/SignUpForm.jsx
+++ cleverwealth/src/components/SignUpForm.jsx
@@ -1,47 +1,73 @@
 import React from 'react';
-import { TextField, Button, Box, Typography } from '@mui/material';
+import { Box, Button, Grid, TextField, Typography } from '@mui/material';
+import { signInWithGoogle } from './config';
 import './SignUpForm.css';
+import signupImage from '../assets/signupImage.png';
 
-const SignUpForm = () => {
+const SignUpForm = ({ onSignUp }) => {
     return (
-        <div className="form-container">
-            <Box className="form-box" sx={{ p: 4, borderRadius: 2 }}>
-                <Typography variant="h4" gutterBottom>Create an Account</Typography>
-                <form noValidate autoComplete="off">
-                    <TextField
-                        fullWidth
-                        label="Full Name"
-                        variant="outlined"
-                        margin="normal"
-                        required
-                    />
-                    <TextField
-                        fullWidth
-                        label="Email Address"
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        type="email"
-                    />
-                    <TextField
-                        fullWidth
-                        label="Password"
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        type="password"
-                    />
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        sx={{ mt: 2, p: 1.5 }}
-                    >
-                        Sign Up
-                    </Button>
-                </form>
-            </Box>
-        </div>
+        <Grid container className="form-container">
+            {/* Left side for the image */}
+            <Grid item xs={12} md={6} className="form-image">
+                <img src={signupImage} alt="Signup" className="signup-image" />
+            </Grid>
+
+            {/* Right side for the sign-up form */}
+            <Grid item xs={12} md={6} className="form-box">
+                <Box sx={{ p: 4, borderRadius: 2 }}>
+                    <Typography variant="h4" gutterBottom>Create an Account</Typography>
+                    <form noValidate autoComplete="off">
+                        <TextField
+                            fullWidth
+                            label="Full Name"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                        />
+                        <TextField
+                            fullWidth
+                            label="Email Address"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            type="email"
+                        />
+                        <TextField
+                            fullWidth
+                            label="Password"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            type="password"
+                        />
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            sx={{ mt: 2, p: 1.5 }}
+                            onClick={onSignUp}
+                        >
+                            Sign Up
+                        </Button>
+
+                        <Typography variant="body2" align="center" sx={{ mt: 2 }}>
+                            OR
+                        </Typography>
+
+                        {/* Google Sign-In Button */}
+                        <Button
+                            fullWidth
+                            variant="outlined"
+                            color="primary"
+                            onClick={signInWithGoogle}
+                            sx={{ mt: 2, p: 1.5 }}
+                        >
+                            Sign in with Google
+                        </Button>
+                    </form>
+                </Box>
+            </Grid>
+        </Grid>
     );
 };
 
